983343477e50d9d5a1bbe8ab64e36dd04cb24c45
hyeon-aa <ahhyun1120@naver.com>
2024-09-06 00:28:49 +0900

    page 이동

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-const Coin = (props) => {
+const Coin = () => {
+    const { coinId } = useParams();
+    console.log('params', coinId)
     return (
         <div>
-            {/* <Link to='/:coinId'>{props.name}</Link > */}
+            dsfsdfs
         </div>
     );
 };
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoins } from "../api";
 import Coin from "./Coin";
-import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
 interface ICoin {
@@ -10,29 +9,30 @@ interface ICoin {
     symbol: string;
 }
 
-
 const Coins = () => {
 
     const { isLoading, data: coins } = useQuery({ queryKey: ["coins"], queryFn: fetchCoins })
     return (
-        <div>
-            {
-                isLoading === true ? 'isloading.....' : coins?.map((coin: ICoin) => {
-                    return (
-                        <>
-                            <div
-                                key={coin.id}>
-                                <Link to='/:coinId'>
-                                    <img src={`https://cryptoicon-api.pages.dev/api/icon/${coin.symbol.toLowerCase()}`} />
-                                    {coin.name}
-                                </Link >
-                            </div>
-                        </>
-                    )
-                })
+        <>
+            <div>
+                {
+                    isLoading === true ? 'isloading.....' : coins?.map((coin: ICoin) => {
+                        return (
+                            <>
+                                <div
+                                    key={coin.id}>
+                                    <Link to={`/${coin.id}`}>
+                                        <img src={`https://cryptoicon-api.pages.dev/api/icon/${coin.symbol.toLowerCase()}`} />
+                                        {coin.name}
+                                    </Link >
+                                </div>
+                            </>
+                        )
+                    })
 
-            }
-        </div>
+                }
+            </div>
+        </>
     );
 };
 
